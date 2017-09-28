3efd8139ab84ff9b370fd3bbbfdeda0df81a2db1
TheRealC05 <31968268+TheRealC05@users.noreply.github.com>
2017-09-28 21:07:05 +0200

    LDA varianty naimplementovane

--- Cpu6502.go
+++ Cpu6502.go
@@ -12,8 +12,8 @@ var memory [65536]uint8
 
 func main() {
 	// LDA $23
-	memory[0x0000] = 0xA9
-	memory[0x0001] = 0x23
+	memory[0x0000] = 0xA5
+	memory[0x0001] = 0x00
 	memory[0x0002] = 0x00
 	memory[0x0003] = 0x00
 	memory[0x0004] = 0x00
@@ -23,16 +23,70 @@ func main() {
 }
 
 func opLdaImmediate() {
-	// LDA $23
-	// op code = $A9
+	// LDA #23
+	// opcode = $A9
 	a = memory[pc + 1]
-	pc = pc + 2
+	pc += 2
 }
 
 func opLdaZeroPage() {
-	// LDA [$80]
-	// op code = $A5
+	// LDA $80
+	// opcode = $A5
 	var address = memory[pc + 1]
 	a = memory[address]
-	pc = pc + 2
+	pc += 2
+}
+
+func opLdaZeroPageX() {
+	// LDA $80, X
+	// opcode = $B5
+	var address = memory[pc + 1]
+	a = memory[address + x]
+	pc += 2
+}
+
+func opLdaAbsolute() {
+	// LDA $1080
+	// opcode = $AD
+	var address = uint16(memory[pc + 1]) + uint16(memory[pc + 2]) * 256
+	a = memory[address]
+	pc += 3
+}
+
+func opLdaAbsoluteX() {
+	// LDA $1080, X
+	// opcode = $BD
+	var address = uint16(memory[pc + 1]) + uint16(memory[pc + 2]) * 256
+	a = memory[address + uint16(x)]
+	pc += 3
+}
+
+func opLdaAbsoluteY() {
+	// LDA $1080, Y
+	// opcode = $B9
+	var address = uint16(memory[pc + 1]) + uint16(memory[pc + 2]) * 256
+	a = memory[address + uint16(y)]
+	pc += 3
+}
+
+func opLdaIndexedIndirect() {
+	// LDA ($80, X)
+	// opcode = $A1
+	var addressOfAddress = memory[pc + 1]
+	var addressLo = memory[addressOfAddress + x]
+	var addressHi = memory[addressOfAddress + x + 1]
+	var address = uint16(addressLo) + uint16(addressHi) * 256
+	a = memory[address]
+	pc += 2
+}
+
+func opLdaIndirectIdexed() {
+	// LDA ($80), Y
+	// opcode = $B1
+	var addressOfAddress = memory[pc + 1]
+	var addressLo = memory[addressOfAddress]
+	var addressHi = memory[addressOfAddress + 1]
+	var address = uint16(addressLo) + uint16(addressHi) * 256
+	a = memory[address + uint16(y)]
+	pc += 2
 }
